80a15b1cdf26901fe9c938911e9ac46cdaadccee
Mark Czirr <shutdownemergency@gmail.com>
2026-02-23 18:02:08 -0500

    add budgetcategoryseeder

---
+++ database/seeders/BudgetCategorySeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BudgetCategory;
+use App\Models\RecipeTag;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class BudgetCategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $admin = User::first();
+
+        // --- Budget categories ---
+        $categories = collect([
+            ['name' => 'Housing', 'color' => 'blue'],
+            ['name' => 'Utilities', 'color' => 'cyan'],
+            ['name' => 'Groceries', 'color' => 'emerald'],
+            ['name' => 'Transportation', 'color' => 'amber'],
+            ['name' => 'Insurance', 'color' => 'violet'],
+            ['name' => 'Entertainment', 'color' => 'pink'],
+            ['name' => 'Healthcare', 'color' => 'rose'],
+            ['name' => 'Subscriptions', 'color' => 'orange'],
+        ])->map(fn ($cat, $i) => BudgetCategory::factory()->create([
+            'user_id' => $admin->id,
+            'name' => $cat['name'],
+            'color' => $cat['color'],
+            'sort_order' => $i,
+        ]));
+    }
+}
